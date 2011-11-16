ffc69c55f8ddf2b064ba94e6a5a667f6958be09c
passionateprogrammersguild <mmann2943@gmail.com>
2011-11-15 20:34:43 -0500

    Added a target to run nodejs and execute the test scripts

--- src/ZombieAction/spec/zombie_spec.js
+++ src/ZombieAction/spec/zombie_spec.js
@@ -1,13 +1,16 @@
-﻿var name = 'Michael',
-    zombie = {};
+﻿var zombieFactory = require('zombie');
+
+var name = 'Michael',
+    zombie;
 
 beforeEach(function () {
-    zombie = new Zombie(name);
+    zombie = zombieFactory.CreateZombie(name);
 });
 
 describe("zombie test suite", function () {
     
     it("should say I want to eat your brains", function () {
-        expect(zombie.sayHello()).toBe(name + 'wants to eat your brains');
+        
+        expect(zombie.sayHello()).toBe(name + ' wants to eat your brains');
     });
 });
