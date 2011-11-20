853fab4b43585d660a84ea7c7af7855d8205cdbe
passionateprogrammersguild <mmann2943@gmail.com>
2011-11-20 17:33:23 -0500

    Added some more to the presentation in terms of i images and content

--- presentation/index.js
+++ presentation/index.js
@@ -11,3 +11,22 @@ $(function() {
 	});
 });
 
+var presentationHelper = (function() {
+    var map = {};
+    
+    map['continuousLearner.jpg.next'] = {src:'softwareDeveloper.jpg', alt:'Developer'};
+    map['softwareDeveloper.jpg.next'] = {src:'father.jpg', alt:'Father'};
+    map['softwareDeveloper.jpg.prev'] = map['continuousLearner.jpg.next']; 
+    map['father.jpg.next'] = {src:'grandfather.jpg', alt:'Grand Father'};
+    
+        return {
+            getImage: function(curImgSrc, direction) {
+                 var mapKey = curImgSrc + '.' + direction || 'next',
+                     obj = map[mapKey];
+                     
+                //var idx = to - offset;
+                //return idx >= 0 && idx <= map.length -1 ? map[idx] : {src='#', alt='no image found'};
+                return obj;
+            }
+        };
+}());
