0096ce0bc03127c1f4438067452e5b6f33a3cb09
passionateprogrammersguild <mmann2943@gmail.com>
2011-11-22 02:16:52 -0500

    Did more work on the presentation in terms of adding some more slides and content

--- presentation/index.js
+++ presentation/index.js
@@ -10,23 +10,3 @@ $(function() {
 		$('#transition-theme-link').attr('href', $(this).val());
 	});
 });
-
-var presentationHelper = (function() {
-    var map = {};
-    
-    map['continuousLearner.jpg.next'] = {src:'softwareDeveloper.jpg', alt:'Developer'};
-    map['softwareDeveloper.jpg.next'] = {src:'father.jpg', alt:'Father'};
-    map['softwareDeveloper.jpg.prev'] = map['continuousLearner.jpg.next']; 
-    map['father.jpg.next'] = {src:'grandfather.jpg', alt:'Grand Father'};
-    
-        return {
-            getImage: function(curImgSrc, direction) {
-                 var mapKey = curImgSrc + '.' + direction || 'next',
-                     obj = map[mapKey];
-                     
-                //var idx = to - offset;
-                //return idx >= 0 && idx <= map.length -1 ? map[idx] : {src='#', alt='no image found'};
-                return obj;
-            }
-        };
-}());
